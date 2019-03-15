c83f5e0dad785235465f8e0db20d7a61fc0a2706
Ed McCormic <edmccormic@gmail.com>
2019-03-15 14:40:05 -0600

    adding Pojos. Closes #2.

---
+++ src/main/java/com/switbeard/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.switbeard.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/switbeard/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.switbeard.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
